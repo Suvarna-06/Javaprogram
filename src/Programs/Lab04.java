19858d529e5a7a1861cc66ca03cfbb812d365375
Sandhya <sandhyamohansankeshwar@gmail.com>
2025-01-15 15:54:37 +0530

    11th jan 2025

---
+++ src/Programs/Lab04.java
@@ -0,0 +1,14 @@
+package Programs;
+
+public class Lab04 {
+    public static void main(String[] args) {
+        int _ = 56;
+        int $ = 78;
+        // int -> primitive - data type- numeric
+        // it stores the whole number - without any decimal point
+        // age -> variable name or identifier
+
+        // = operator assignment
+        // 65 = literal( int) value
+    }
+}
---
+++ src/Programs/Lab05_Interview_QA.java
@@ -0,0 +1,8 @@
+package Programs;
+
+public class Lab05_Interview_QA {
+    public static void main(String[] args) {
+//        int enum = 10;
+//        System.out.println(enum);
+    }
+}
