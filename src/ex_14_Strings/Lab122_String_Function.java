3ba0ec9caef80dead68d2b3f0bcb10f67df5e6e1
Sandhya <sandhyamohansankeshwar@gmail.com>
2025-02-10 11:17:34 +0530

    26th Jan 2025 Part 2

--- src/ex_14_Strings/Lab122_String_Function.java
+++ src/ex_14_Strings/Lab122_String_Function.java
@@ -2,6 +2,12 @@ package ex_14_Strings;
 
 public class Lab122_String_Function {
     public static void main(String[] args) {
+            String another_palindrome = "Niagara. O roar again!";
+            String roar=another_palindrome.substring(11,15);
+            System.out.println(roar);
 
+            String s1 ="Sandhya";
+            String s2 = s1.concat("Sankeshwar");
+            System.out.println(s2);
     }
 }
