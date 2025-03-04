d5120c49c6ec4d364e237dc540d4bc1475d3c51b
Suvarna-06 <sandhyasankesh@gmail.com>
2025-03-04 23:41:12 +0530

    4rd March 2025

---
+++ src/Interview_Programs/GradeClaculator.java
@@ -0,0 +1,23 @@
+package Interview_Programs;
+
+public class GradeClaculator {
+    public static void main(String[] args) {
+        int score = 100;
+        char grade;
+
+        if(score>=90 && score<=100){
+            grade = 'A';
+        } else if (score>=80 && score<=89) {
+            grade='B';
+            
+        } else if (score>=70 && score<=79) {
+            grade ='C';
+        } else if (score>=60 && score<=69) {
+            grade ='D';
+        }
+        else {
+            grade='F';
+        }
+        System.out.println(" Your grade is "+grade);
+    }
+}
